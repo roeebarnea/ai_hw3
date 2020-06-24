5bd0508212386df3ed57acd22c47fc2297e0adef
roeebarnea <roeebarnea@campus.technion.ac.il>
2020-06-24 20:06:50 +0300

    finish ID3 - not working yet

--- DT.py
+++ DT.py
@@ -1,8 +1,121 @@
 import pandas as pd
+import sklearn as skl
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
 
+class Node:
+    def __init__(self, features, examples):
+        self.s1 = None
+        self.s2 = None
+        self.classification = None
+        self.f = None
+        self.features = features
+        self.examples = examples
 
+def selectFeature(features, examples, df):
+    total_e = len(examples)
+    max_IG, best_f= 0, None
+    for f in features:
+        mid = (df[f].max()-df[f].min())/2
+        count_s1, count_s2 = 0, 0
+        count_s1_t, count_s1_f, count_s2_t, count_s2_f = 0, 0, 0, 0
+        for e in examples:
+            if df[f][e] < mid:
+                count_s1 += 1
+                if df["diagnosis"][e] == 1:
+                    count_s1_t += 1
+                else:
+                    count_s1_f += 1
+            else:
+                count_s2 += 1
+                if df["diagnosis"][e] == 1:
+                    count_s2_t += 1
+                else:
+                    count_s2_f += 1
+        if count_s1_t == 0 or count_s1_t == 0 or count_s1==0:
+            H1 =0
+        else:
+            rat_s1_t = count_s1_t / count_s1
+            rat_s1_f = count_s1_f / count_s1
+            H1 = -(rat_s1_t * np.log2(rat_s1_t) + rat_s1_f * np.log2(rat_s1_f))
+        if count_s2_t == 0 or count_s2_t == 0 or count_s2==0:
+            H2 =0
+        else:
+            rat_s2_t = count_s2_t / count_s2
+            rat_s2_f = count_s2_f / count_s2
+            H2 = -(rat_s2_t * np.log2(rat_s2_t) + rat_s2_f * np.log2(rat_s2_f))
+
+        IG = 1 - ( (count_s1/total_e)*H1 + (count_s2/total_e)*H2 )
+        if IG > max_IG:
+            max_IG = IG
+            best_f = f
+    return best_f
+
+def majorityClass(examples, df):
+    total = len(examples)
+    count_t, count_f = 0, 0
+    for e in examples:
+        if df["diagnosis"][e] == 1:
+            count_t += 1
+        else:
+            count_f += 1
+    if count_t >= count_f:
+        return 1 , count_t/total
+    else:
+        return 0 , count_f/total
+
+def new_node_s1(examples, features, f):
+    new_examples = []
+    mid = (df[f].max() - df[f].min()) / 2
+    for e in examples:
+        if df[f][e] < mid:
+            new_examples.append(e)
+    new_features = features.drop([f])
+    return Node(new_features, new_examples)
+
+def new_node_s2(examples, features, f):
+    new_examples = []
+    mid = (df[f].max() - df[f].min()) / 2
+    for e in examples:
+        if df[f][e] >= mid:
+            new_examples.append(e)
+    new_features = features.drop([f])
+    return Node(new_features, new_examples)
+
+def ID3(cur_node, df):
+    if len(cur_node.examples) == 0:
+        return
+    c = majorityClass(cur_node.examples, df)
+    cur_node.classification = c[0]
+    if c[1] == 1 or len(cur_node.features) == 0:
+        return
+    cur_node.f = selectFeature(cur_node.features, cur_node.examples, df)
+    cur_node.s1 = new_node_s1(cur_node.examples, cur_node.features, cur_node.f)
+    cur_node.s2 = new_node_s2(cur_node.examples, cur_node.features, cur_node.f)
+
+def get_classifier_ID3(df):
+    features = df.keys().drop(['diagnosis'])
+    examples = list(range(0, len(df['diagnosis'])-1))
+    tree = Node(features, examples)
+    ID3(tree,df)
+    return tree
 
 if __name__ == "__main__":
+    # data = pd.read_csv("test.csv")
+    # df = pd.DataFrame(data)
+    # keys = df.keys().copy()
+    # #keys.delete()
+    # l =keys.drop(['diagnosis'])
+    # print(l)
+    # print(keys)
+    # print(type(keys))
+    # print(df['area_mean'][0])
+    # t_max = df['area_mean'].max()
+    # t_min = df['area_mean'].min()
+    # print(t_max, " ", t_min)
+    # print(len(df['area_mean']))
+
     data = pd.read_csv("test.csv")
     df = pd.DataFrame(data)
-    print(df['area_mean'][0])
+    tree = get_classifier_ID3(df)
+    print(tree)
